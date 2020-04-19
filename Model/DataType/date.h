c9a52fd19ea6fcebefeb64311c560c1768bc6675
student <egongalvani@libero.it>
2020-04-19 23:23:18 +0200

    creati header date e time

--- Model/DataType/date.h
+++ Model/DataType/date.h
@@ -1,4 +1,46 @@
 #ifndef DATE_H
 #define DATE_H
 
+#include <iostream>
+
+class Date {
+private:
+    unsigned short _day, _month, _year;
+public:
+    Date(); // ora attuale
+    Date(unsigned short, unsigned short, unsigned short); // anno, mese e giorno
+
+    void addDays(unsigned short);
+    void addMonths(unsigned short);
+    void addYears(unsigned short);
+
+    unsigned short day() const;
+    unsigned short dayOfWeek() const;
+    unsigned short dayOfYear() const;
+    unsigned short daysInMonth() const;
+    unsigned short daysInYear() const;
+
+    unsigned short month() const;
+    unsigned short year() const;
+
+    unsigned int daysTo(const Date& d) const;
+
+    unsigned short secSinceStartOfDay() const;
+    std::string toString(const std::string&) const;
+
+    bool operator!=(const Date&) const;
+    bool operator==(const Date&) const;
+    bool operator<(const Date&) const;
+    bool operator<=(const Date&) const;
+    bool operator>(const Date&) const;
+    bool operator>=(const Date&) const;
+
+    static Date currentDate();
+    static Date fromString(const std::string&);
+    static bool isLeapYear(unsigned short);
+};
+
+std::ostream& operator<<(std::ostream&, const Date&);
+std::istream& operator>>(std::ostream&, Date&);
+
 #endif // DATE_H
--- Model/DataType/time.h
+++ Model/DataType/time.h
@@ -1,4 +1,40 @@
 #ifndef TIME_H
 #define TIME_H
 
+#include <iostream>
+
+class Time {
+private:
+    unsigned int _sec;
+public:
+    Time(); // ora attuale
+    Time(unsigned int, unsigned int, unsigned int = 0); // ore, minuti e secondi
+
+    void addHours(unsigned int);
+    void addMinutes(unsigned int);
+    void addSeconds(unsigned int);
+
+    unsigned int hour() const;
+    unsigned int minute() const;
+    unsigned int second() const;
+
+    unsigned int secSinceStartOfDay() const;
+    std::string toString(const std::string&) const;
+
+    bool operator!=(const Time&) const;
+    bool operator==(const Time&) const;
+    bool operator<(const Time&) const;
+    bool operator<=(const Time&) const;
+    bool operator>(const Time&) const;
+    bool operator>=(const Time&) const;
+
+    static Time currentTime();
+};
+
+std::ostream& operator<<(std::ostream&, const Time&);
+std::istream& operator>>(std::ostream&, Time&);
+
+
+
+
 #endif // TIME_H
