ee2f73b904b523c2b27d1b54da5154aa136fd88a
Daniele <danielegiachetto1999@gmail.com>
2020-05-21 02:35:47 +0200

    Separazione header & source factory

--- CuteCalendar.pro
+++ CuteCalendar.pro
@@ -23,6 +23,7 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    Model/Hierarchy/factory.cpp \
     View/modifydialog.cpp \
     View/modview.cpp \
     View/viewallenamento.cpp \
--- Model/Hierarchy/factory.h
+++ Model/Hierarchy/factory.h
@@ -30,83 +30,16 @@ private:
     std::istringstream temp;
     int id;
 
-    void firstParse() {
+    void firstParse();
 
-        //MANCA TODOLIST
-        //Ricavo dei campi dati di evento
-        nome = json["NAME"].toString().toStdString();
-        //conversione stringa -> data
-        temp= std::istringstream(json["DATA"].toString().toStdString());
-        std::cout<<std::endl<<"--DATA" <<json["DATA"].toString().toStdString()<<std::endl;
-        temp>>date;
-        descr = json["DESCRIPTION"].toString().toStdString();
-        place = json["PLACE"].toString().toStdString();
-        //conversione jsonarray -> array stringhe
-        tmp = json["TAGS"].toArray();
-        for (QJsonArray::const_iterator it = tmp.begin();it!=tmp.end();++it) {
-            tags->push_back(it->toString().toStdString());
-        }
-
-    }
-
-    void secondParse() {
-
-        if (id!=BirthDay::ID) {
-            temp= std::istringstream(json["START_TIME"].toString().toStdString());
-            temp>>start;
-            temp= std::istringstream(json["END_TIME"].toString().toStdString());
-            temp>>end;
-            if (id!=Workout::ID) {
-                rep = json["REPEAT"].toBool();
-                //conversione stringa -> time
-                temp= std::istringstream(json["ALERT_TIME"].toString().toStdString());
-                temp>>alert;
-            }
-        }
-
-    }
+    void secondParse();
 
 public:
 
-    Factory(QJsonObject &js) : json(js) {
-        tags = new std::vector<std::string>();
-        vectPart = std::vector<std::string>();
-
-    }
+    Factory(QJsonObject &js);
 
 
-    Event* parse() {
-
-        //USO stoi e non .toInt perchè toInt ritorna sempre valore di default
-        id = std::stoi(json["ID"].toString().toStdString());
-
-        firstParse();
-        secondParse();
-
-        switch (id) {
-        case Reminder::ID:
-            //REMINDER = 1
-            urg = json["URGENCY"].toInt();
-            return new Reminder(urg,start,end,alert,rep,nome,descr,place,date,tags);
-        case Workout::ID:
-            //WORKOUT int x,Time start,Time end
-            act = json["ACTIVITY"].toInt();
-            return new Workout(act,start,end,nome,descr,place,date,tags);
-        case Meeting::ID:
-            //MEETING
-            tmp = json["PARTECIPANTS"].toArray();
-            for (QJsonArray::const_iterator it = tmp.begin();it!=tmp.end();++it) {
-                vectPart.push_back(it->toString().toStdString());
-            }
-            return new Meeting(vectPart,start,end,alert,rep,nome,descr,place,date,tags);
-        case BirthDay::ID:
-            //BIRTHDAY
-            temp= std::istringstream(json["DATA_NASCITA"].toString().toStdString());
-            temp>>birth;
-            return new BirthDay(birth,nome,descr,place,date,tags);
-        }
-        return nullptr;
-    }
+    Event* parse();
 };
 
 #endif // FACTORY_H
