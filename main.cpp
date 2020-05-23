d4e04f0d50f24bfde58c0d69589b38380fad8140
Daniele <danielegiachetto1999@gmail.com>
2020-05-23 16:05:18 +0200

    clean up main

--- main.cpp
+++ main.cpp
@@ -1,36 +1,5 @@
 #include <QApplication>
-#include <QFile>
-#include <QJsonDocument>
-#include <iostream>
-
 #include "View/mainwindow.h"
-#include "Model/DataType/date.h"
-#include "Model/DataType/time.h"
-#include "Model/Container/unorderedmultimap.h"
-#include "Model/Hierarchy/event.h"
-#include "Model/Hierarchy/workout.h"
-#include "Model/Hierarchy/reminder.h"
-#include "Model/Hierarchy/meeting.h"
-
-void writeToDisk(const QString path,const QJsonDocument & obj) {
-    try {
-        QFile file;
-        file.setFileName(path);
-        file.open(QIODevice::WriteOnly | QIODevice::Text);
-        file.write(obj.toJson());
-    }catch (...) {}
-}
-
-QJsonDocument readFromDisk(const QString path) {
-    QFile loadFile(path);
-    if (!loadFile.open(QIODevice::ReadOnly)) {
-        qWarning("Couldn't open save file.");
-    }
-
-    QByteArray saveData = loadFile.readAll();
-    QJsonDocument loadDoc(QJsonDocument::fromJson(saveData));
-    return loadDoc;
-}
 
 int main(int argc, char *argv[]) {
     std::cout << "ciao" << std::endl;
@@ -40,4 +9,3 @@ int main(int argc, char *argv[]) {
     w.show();
     return a.exec();
 }
-
