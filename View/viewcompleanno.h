6f26e9570a6777b9aa4f44ec72633660a7f6af6d
Valton <tahirajvalton@mail.com>
2020-05-25 21:05:07 +0200

    Prima modifica ViewCompleanno

--- View/viewcompleanno.h
+++ View/viewcompleanno.h
@@ -2,16 +2,23 @@
 #define VIEWCOMPLEANNO_H
 
 #include "modview.h"
+#include <QSpinBox>
 #include "../Model/Hierarchy/birthday.h"
 
 class ViewCompleanno : public ModView {
     Q_OBJECT
 
-   public:
+    public:
       explicit ViewCompleanno(QWidget *parent = 0);
       ~ViewCompleanno() = default;
 
       // BirthDay* createEvent() override;
+       QSpinBox* annoNascita;
+       QLabel* nascita;
+       void setEnabled(bool) override;
+       void pushSaves(Model::It) override;
+       void fillView(Model::It) override;
+
 };
 
 #endif // VIEWCOMPLEANNO_H
