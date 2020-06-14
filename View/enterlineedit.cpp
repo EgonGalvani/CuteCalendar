267e30070ce2ceae5e70a9abe1a922a72238719c
student <egongalvani@libero.it>
2020-06-14 20:45:38 +0200

    commentato enterlinedit

--- View/enterlineedit.cpp
+++ View/enterlineedit.cpp
@@ -6,6 +6,9 @@ EnterLineEdit::EnterLineEdit(QWidget* parent)
     : QLineEdit(parent) {}
 
 void EnterLineEdit::keyPressEvent(QKeyEvent* event) {
+
+    // se l'evento è quello di interesse (click bottone enter), viene emesso un signal
+    // altrimenti si comporta come QLineEdit
     if(event->key() == Qt::Key_Return)
         emit enterKeyPressed();
     else QLineEdit::keyPressEvent(event);
--- View/enterlineedit.h
+++ View/enterlineedit.h
@@ -4,11 +4,14 @@
 #include <QLineEdit>
 #include <QWidget>
 
+// QLineEdit con signal per il click del bottone invio
 class EnterLineEdit : public QLineEdit {
     Q_OBJECT
 
 public:
     EnterLineEdit(QWidget* = nullptr);
+
+    // ridefinizione del metodo keyPressEvent della classe QLineEdit
     virtual void keyPressEvent(QKeyEvent*);
 
 signals:
