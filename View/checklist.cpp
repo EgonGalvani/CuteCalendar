3e94257cbe34f59a125b61b457bac0dd39ee8490
student <egongalvani@libero.it>
2020-06-14 20:43:01 +0200

    Aggiunti commenti checklist

--- View/checklist.cpp
+++ View/checklist.cpp
@@ -10,23 +10,27 @@ CheckList::CheckList(QWidget* parent)
     setSpacing(4);
 }
 
+// inserimento di un nuovo item nella checklist
 void CheckList::addItem(const QString& label, bool checked) {
     QString itemLabel = label.trimmed();
-    if(!itemLabel.isEmpty()) {
 
-        if(!hasItem(itemLabel)) {
+    if(!itemLabel.isEmpty()) { // controllo che la label dell'item non sia vuota
+        if(!hasItem(itemLabel)) { // controllo che non esista già l'item nella lista
+
+            // aggiunta dell'item nella lista
             QListWidgetItem* item = new QListWidgetItem;
             QListWidget::addItem(item);
             QCheckBox *checkBox = new QCheckBox(itemLabel, this);
             checkBox->setChecked(checked);
             setItemWidget(item, checkBox);
-            scrollToItem(item);
+            scrollToItem(item); // faccio lo scroll della lista per mostrare il nuovo item inserito
         } else
             throw std::logic_error("Elemento già presente nella lista");
     } else
         throw std::logic_error("La stringa da inserire non può essere vuota");
 }
 
+// controllo se un item con la stringa indicata è già presente nella lista
 bool CheckList::hasItem(const QString &label) const {
     bool found = false;
     for(int i = 0; i < count() && !found; i++) {
@@ -36,6 +40,8 @@ bool CheckList::hasItem(const QString &label) const {
     return found;
 }
 
+// viene restituito un vector contenente dei pair che permettono
+// di rappresentare correttamente un item della lista, formato da: testo e stato (checked o unchecked)
 std::vector<std::pair<std::string, bool>>
 CheckList::getStatus() const {
     std::vector<std::pair<std::string, bool>> list;
--- View/checklist.h
+++ View/checklist.h
@@ -6,12 +6,20 @@
 #include <QWidget>
 #include <vector>
 
+// widget per la gestione di una checklist, ogni item della lista
+// è formato da una stringa di testo e una checkbox
 class CheckList : public QListWidget {
 private:
+
+    // controlla se un item con una label specifica è presente nella lista
     bool hasItem(const QString& label) const;
 public:
     CheckList(QWidget* = nullptr);
+
+    // aggiunge un item nella lista
     void addItem(const QString &label, bool = false);
+
+    // ottiene un vettore che rappresenta la lista di item della checkbox
     std::vector<std::pair<std::string, bool>> getStatus() const;
 
      ~CheckList() = default;
